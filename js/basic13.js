14b8fc0598f5f9833145a87fcf504e79d42e86f7
Olivia Morgan <morgan.olivia@gmail.com>
2018-01-30 16:48:38 -0500

    Finsih #8

--- js/basic13.js
+++ js/basic13.js
@@ -41,8 +41,8 @@ function printArrayValues(array) {
   }
 } 
 
-testArray = [0, 2, 1, 3, 5, 4];
-printArrayValues(testArray);
+testArray1 = [0, 2, 1, 3, 5, 4];
+printArrayValues(testArray1);
 // 5 Print max value of a given array
 
 function maxValue(array) {
@@ -55,8 +55,8 @@ function maxValue(array) {
   console.log(max);
 }
 
-testArray = [0, 2, 1, 3, 5, 4];
-maxValue(testArray);
+testArray2 = [0, 2, 1, 3, 5, 4];
+maxValue(testArray2);
 // 6 Print the average of the values of a given array
 
 function average(array) {
@@ -67,8 +67,8 @@ function average(array) {
   console.log(sum/array.length);
 }
 
-testArray = [0, 2, 1, 3, 5, 4]
-average(testArray);
+testArray3 = [0, 2, 1, 3, 5, 4]
+average(testArray3);
 
 // 7 Create and return an array with all odd integers from 1-255
 
@@ -84,18 +84,30 @@ function makeOddArray() {
 
 makeOddArray();
 
-// // 8 Square each value of a given array then return that array with the new values
+// 8 Square each value of a given array then return that array with the new values
 
-// def square_array(array)
-//   squared_array = []
-//   array.each do |num|
-//     square = num * num
-//     squared_array << square
-//   end
-//   squared_array
-// end
+//creating new array and pushing squared values
+function squareArray(array) {
+  let squaredArray = []
+  for(i = 0; i < array.length; i++) {
+    squaredArray.push(array[i] * array[i])
+  }
+  return squaredArray;
+}
 
-// // 9 Return all values of a given array that are greater than a given value
+//changing values of current array by squaring them
+function squareTheArray(array) {
+  for(i = 0; i < array.length; i++) {
+    array[i] = array[i] * array[i]
+  }
+  return array;
+} 
+
+testArray4 = [0, 2, 1, 3, 5, 4]
+squareArray(testArray4);
+squareTheArray(testArray4);
+
+// 9 Return all values of a given array that are greater than a given value
 
 // def greater(array, value)
 //   greater_array = []
@@ -106,6 +118,11 @@ makeOddArray();
 //   end
 //   greater_array
 // end
+
+function greater(array, value) {
+  let greaterArray = [];
+
+}
 
 // // 10 Return the given array after setting any negative values to zero
 
